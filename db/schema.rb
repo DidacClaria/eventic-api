e296a25c102308bf5df0cefec4850f48b6510895
AleixLinares <aleix.linares@estudiantat.upc.edu>
2021-04-12 18:43:49 +0200

    Prova funcionament

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,28 @@
 
 ActiveRecord::Schema.define(version: 2021_04_08_142744) do
 
+  create_table "companies", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "username"
+    t.string "logo"
+    t.string "email"
+    t.string "language"
+    t.string "nif"
+    t.text "tokens"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_companies_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_companies_on_uid_and_provider", unique: true
+  end
+
   create_table "eventos", force: :cascade do |t|
     t.string "title"
     t.string "description"

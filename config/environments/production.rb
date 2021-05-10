76cabfd3a755bcc030efc613fe7375e051d5e2aa
AleixLinares <aleix.linares@estudiantat.upc.edu>
2021-05-10 18:13:40 +0200

    pls host 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,18 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => "eventic-api.herokuapp.com", :protocol => 'https'}
 
+  config.action_mailer.delivery_method= :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "eventic.webmaster@gmail.com",
+    password: "Pandevs123" #<-- PER FER TEST CONTRASENYA AQUI
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
